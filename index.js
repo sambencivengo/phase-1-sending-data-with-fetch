72764d7efab3fb815f54ee13676779a13a17f188
Sam Bencivengo <sambencivengo@gmail.com>
2021-10-03 17:56:52 -0400

    commit tree 76cddc87d151a67d2bdc2269d444db2a30d25f24

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+const dogURL = 'http://localhost:3000/dogs';
+const catURL = 'http://localhost:3000/cats';
+const usersURL = 'http://localhost:3000/users';
+const robotsURL = 'http://localhost:3000/robots';
+
+function submitData(userName, userEmail) {
+  fetch(usersURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  });
+}
